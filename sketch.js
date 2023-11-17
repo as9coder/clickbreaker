770273535bf42c760d6fd3d7b7c228636ab7c458
as9coder <135017614+as9coder@users.noreply.github.com>
2023-11-17 19:04:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var click, clickImg;
+var count=1000000;
+
+function preload(){
+  clickImg= loadImage("click.gif");
+}
+
+function setup() {
+  createCanvas(windowWidth-8, windowHeight-8);
+
+  click= createSprite(width/2, height/2, 20,20);
+  click.addImage(clickImg);
+  click.scale= 0.2;
+
+  count= localStorage.getItem('count')|| 1000000;
+}
+
+function draw() 
+{
+  background(30);
+
+  textSize(30);
+  fill("red");
+  text("CLICK ME", width/2-60, height/2+100);
+  textSize(20);
+  text("CLICKED: "+count+" Times", width-250, height/9);
+
+  if(mousePressedOver(click)){
+    count-=1;
+    local();
+  }
+
+  if(count<=0){
+    text("YOU ARE EXTRAORDINARY", width/2, height-100);
+  }
+
+  local();
+
+  drawSprites();
+}
+
+function local(){
+  localStorage.setItem('count',count);
+}
+
+
+
+
